1954510eb41e645dfa367ac75476d760a9bcf51b
vishaldeepak <vishal.deepak@outlook.com>
2019-04-04 12:42:07 +0530

    Cleaned up specs with proper naming

--- spec/support/shared_examples/authorization.rb
+++ spec/support/shared_examples/authorization.rb
@@ -1,11 +1,3 @@
-RSpec.shared_examples "status ok respsone" do
-  it { expect(response).to have_http_status(200) }
-end
-
-RSpec.shared_examples "status created respsone" do
-  it { expect(response).to have_http_status(201) }
-end
-
 RSpec.shared_examples "a authorized action" do
   context "when the token is invalid" do
     before {
