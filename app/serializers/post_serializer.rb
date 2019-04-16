44df27c5d6b70555921b6193d9b0e3f3fe0cb379
vishaldeepak <vishal.deepak@outlook.com>
2019-04-16 15:36:28 +0530

    Return posts created at in proper format

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,8 @@
 class PostSerializer
   include FastJsonapi::ObjectSerializer
+
+  extend ActionView::Helpers::DateHelper
+
   set_key_transform :camel_lower
 
   set_id :id
@@ -7,6 +10,10 @@ class PostSerializer
 
   attribute :created_by do |object|
     "#{object.user.username}"
+  end
+
+  attribute :created_at do |object|
+    "#{time_ago_in_words(object.created_at)} ago"
   end
 
   belongs_to :user
